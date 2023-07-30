6b23db560f61f9742b7053246cb6ec5f415ddb63
Irshad <irshadyousuf0075@gmail.com>
2023-07-30 10:44:30 -0700

    modification done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRouter = require("./User/userRouter.js");
-const productRouter = require("./Products/productRouter.js"); // Add the productRouter
+const productRouter = require("./Products/productRouter.js");
 
 dotenv.config();
 
